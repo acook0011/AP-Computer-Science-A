8165df5c294e1b72db2272f62e671c897141bb76
Avery Cook <acook0011@myduneland.org>
2018-09-06 17:53:03 -0500

    Completed Chapter 4 Programs 1

---
+++ Ch4Prog1/Division.java
@@ -0,0 +1,38 @@
+package Ch4Prog1;
+
+/**
+ * Dividing two numbers and returning quotient and remainder. 
+ *
+ * @aj
+ * @cs4.1
+ */
+
+import java.util.Scanner;
+
+public class Division
+{
+    public static void main(String [] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        int num1, num2;                                // Inputs 
+        int quo, rem;                                  // Outputs - quotient and remainder 
+        
+        // Taking inputs
+        System.out.print("Please enter an integer: ");
+        num1 = reader.nextInt();
+        System.out.print("Please enter another integer: ");
+        num2 = reader.nextInt();
+        
+        // Determination & Calculation
+        if (num1 >= num2){
+            quo = num1 / num2;
+            rem = num1 % num2;
+        }else{
+            quo = num2 / num1;
+            rem = num2 % num1;
+        }
+        
+        // Printing outputs
+        System.out.print("The answer determined after dividing the integers is " + quo + "r. " + rem + ".");
+    }
+}
---
+++ Ch4Prog1/RightTriangle.java
@@ -0,0 +1,48 @@
+package Ch4Prog1;
+
+/**
+ * Determining whether three sides of a triangle are those of a right angle.
+ *
+ * @aj
+ * @cs4.2
+ */
+
+import java.util.Scanner;
+
+public class RightTriangle
+{
+    public static void main(String [] args){
+     
+        Scanner reader = new Scanner(System.in);
+        double a, b, c;                                   // The inputs for the sides of the triangle
+        int right = 0;                                    // Equals 1 if right angle
+        
+        // Taking Inputs
+        System.out.print("Please enter the first side of the triangle: ");
+        a = reader.nextDouble();
+        System.out.print("Please enter the second side of the triangle: ");
+        b = reader.nextDouble();
+        System.out.print("Please enter the third side of the triangle: ");
+        c = reader.nextDouble();
+        
+        // Determining Sides
+        if (c*c == a*a + b*b){
+            right = 1;
+        }else{
+            if (b*b == a*a + c*c){
+                right = 1;
+            }else{
+                if (a*a == b*b + c*c){
+                    right = 1;
+                }
+            }
+        }
+        
+        // Displaying outputs
+        if (right == 1){
+            System.out.print("This is a right triangle.");
+        }else{
+            System.out.print("This is not a right triangle.");
+        }
+    }
+}
