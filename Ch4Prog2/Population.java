a68d36410377cc7f18b90d3098ae6f5cf68c935e
Avery Cook <acook0011@myduneland.org>
2018-09-11 22:17:57 -0500

    Completed Ch4Prog2

--- Ch4Prog2/Population.java
+++ Ch4Prog2/Population.java
@@ -13,11 +13,12 @@ public class Population
     public static void main(String [] args){
     Scanner reader = new Scanner(System.in);
      
-    int popInitial,                // (Input) The starting population of organisms
-        rateGrowth,                // (Input) The rate of growth (a real number greater than 0)
-        rateTime,                  // (Input) The number of hours it takes to achieve the rate of growth
-        timeGrowing,               // (Input) The number of hours in which a population grows
-        popPredict;                // (Output) The predicted population of organisms after time
+    int    popInitial,                // (Input) The starting population of organisms
+           popPredict;                // (Output) The predicted population of organisms after time
+    double rateGrowth,                // (Input) The rate of growth (a real number greater than 0)
+           rateTime,                  // (Input) The number of hours it takes to achieve the rate of growth
+           timeGrowing;               // (Input) The number of hours in which a population grows
+           
          
     // Request the inputs
     System.out.print("The initial population of the organisms: ");
@@ -27,25 +28,28 @@ public class Population
         popInitial = reader.nextInt();
     }
     System.out.print("The rate of growth of the population: ");
-    rateGrowth = reader.nextInt();
+    rateGrowth = reader.nextDouble();
     while (rateGrowth <= 0){
         System.out.print("ERROR: Rate of growth must be a positive integer: ");
-        rateGrowth = reader.nextInt();
+        rateGrowth = reader.nextDouble();
     }
     System.out.print("The number of hours it takes to achieve the rate of growth: ");
-    rateTime = reader.nextInt();
+    rateTime = reader.nextDouble();
     while (rateTime <= 0){
         System.out.print("ERROR: Time for rate of growth must be a positive integer: ");
-        rateTime = reader.nextInt();
+        rateTime = reader.nextDouble();
     }
     System.out.print("The number of hours in which a population will grow: ");
-    timeGrowing = reader.nextInt();
+    timeGrowing = reader.nextDouble();
     while (timeGrowing <= 0){
         System.out.print("ERROR: Time in which a population will grow must be a positive integer: ");
-        timeGrowing = reader.nextInt();
+        timeGrowing = reader.nextDouble();
     }
     
     // Calculations
-    // NOTE: USE RATE OF POPULATION GROWTH FORMULA
+    popPredict = (int) Math.round(popInitial * rateGrowth * (timeGrowing / rateTime) - 0.5);
+    
+    // Display outputs
+    System.out.print("The predicted population, assuming there are no deaths, is " + popPredict);
 }
 }
---
+++ Ch4Prog2/TeacherSalary.java
@@ -0,0 +1,66 @@
+package Ch4Prog2;
+
+/**
+ * The salaries of teachers over a period of time
+ *
+ * @aj
+ * @cs4.10
+ */
+
+import java.util.Scanner;
+public class TeacherSalary
+{
+    public static void main(String [] args){
+     Scanner reader = new Scanner(System.in);
+     
+     double percentIncrease;                  // (Input) The percent increase of salary every year
+     int    salary,                           // (Input) The initial salary 
+            year,                             // (Input) The amount of years with salary
+            yearOverflow,                     // The amount of years over 10
+            yearSub,                          // The subsitution of years if over 10
+            yearCount;                        // The count of how many years have passed
+
+            
+     // Request inputs
+     System.out.print("The salary of the teacher: ");
+     salary = reader.nextInt();
+     while (salary <= 0){
+        System.out.print("ERROR: The initial salary must be a positive integer: ");
+        salary = reader.nextInt();
+    }
+     System.out.print("The percent increase of the salary every year (as a decimal): ");
+     percentIncrease = reader.nextDouble();
+     while (percentIncrease < 0){
+        System.out.print("ERROR: The percent increase cannot be a negative integer: ");
+        percentIncrease = reader.nextDouble();
+    }
+     System.out.print("The total amount of years the salary will be evaluated: ");
+     year = reader.nextInt();
+     while (year <= 0){
+         System.out.print("ERROR: The year must be a positive integer: ");
+         year = reader.nextInt();
+    }
+    
+    
+    // Calculations
+     
+    System.out.println("Year 1 - $" + salary);
+    yearOverflow = year - 10;
+    year -= 1;
+    yearCount = 1;
+    yearSub = 9;
+    while (yearSub > 0){
+            yearSub -= 1;
+            yearCount += 1;
+            salary += Math.round(salary * percentIncrease);
+            System.out.println("Year " + yearCount + " - $" + salary);
+    }if (yearOverflow > 0){                 // Testing to see if the amount of years is over 10
+      while (yearOverflow > 0){            // Teacher receives no more raise after year 10. 
+         yearOverflow -= 1;
+         yearCount +=1;
+         System.out.println("Year " + yearCount + " - $" + salary);
+       }
+    }
+    }
+    
+}
