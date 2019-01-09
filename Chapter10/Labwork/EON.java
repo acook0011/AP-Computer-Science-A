3d891b381e801ac35f5bf0895260ab297a5d3e39
Avery Cook <acook0011@myduneland.org>
2019-01-09 12:30:54 -0600

    Labwork

---
+++ Chapter10/Labwork/EON.java
@@ -0,0 +1,21 @@
+package Chapter10.Labwork;
+
+
+/**
+ * Sort inputs into even, odd, and negative.
+ *
+ * @aj
+ * @10.1
+ */
+
+import java.io.*;
+import java.util.Scanner;
+
+public class EON
+{
+    public static void main(String [] args){
+     Scanner reader = new Scanner(System.in);
+     int[] unlisted = new int[10];
+     int inputs = 0;
+    }
+}
