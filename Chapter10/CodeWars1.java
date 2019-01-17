35972044576ff6bb7b7a09d7bdfc420e1057a187
Avery Cook <acook0011@myduneland.org>
2019-01-17 09:45:02 -0600

    CodeWars

--- Chapter10/CodeWars1.java
+++ Chapter10/CodeWars1.java
@@ -15,23 +15,21 @@ public class CodeWars1
 {
     public static void main(String [] args) throws IOException {
         Scanner reader = new Scanner(new File("water.txt"));
-        int[] gallon = new int[10];
+        int[] gallon = new int[5];
         int count = 0;
+        String weekAmount;
         
-        while(reader.hasNext()){
+        while(count < gallon.length && reader.hasNext()){
             int number = reader.nextInt();
-            if (number != 0){
             gallon[count] = number;
             System.out.println(gallon[count] + " gallons per week will last " + 
-                               weeklyRate(gallon[count]) + " week.");
+                               weeklyRate(gallon[count]) + "week.");
             count++;
-        }else{
-            break;
         }
-    }
     }
     
     static int weeklyRate(int gallon){
+        // Set variables & count for first week
         int currentGallons = 10000-gallon;
         int week = 1;
         while(currentGallons > 0){
