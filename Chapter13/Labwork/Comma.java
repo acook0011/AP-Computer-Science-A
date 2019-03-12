c36c46edadbd4b076a4b3b22c82ec16e2c6e8a61
Avery Cook <acook0011@myduneland.org>
2019-03-12 12:38:39 -0500

    Corrections

--- Chapter13/Labwork/Comma.java
+++ Chapter13/Labwork/Comma.java
@@ -13,14 +13,15 @@ public class Comma
         System.out.println(insertComma(1000));
         System.out.println(insertComma(10020));
         System.out.println(insertComma(100020));
-        System.out.println(insertComma(1000020));
+        System.out.println(insertComma(1000320));
         System.out.println(insertComma(999));
+        System.out.println(insertComma(700));
         System.out.println(insertComma(2));
         System.out.println(insertComma(2222222));
         System.out.println(insertComma(7773453));
         System.out.println(insertComma(436789));
         System.out.println(insertComma(436789321));
-        System.out.println(insertComma(400009700));
+        System.out.println(insertComma(400300700));
         System.out.println(insertComma(555100234));
     }
     
@@ -35,7 +36,7 @@ public class Comma
         if (n % 1000 == 0) {
             return insertComma(n/1000) + ",000";
         }
-        if ((n/10) % 10 == 0) {
+        if ((n/10) % 100 == 0) {
             return insertComma(n/1000) + ",00" + Integer.toString(n%10);
         }
         if ((n/100) % 10 == 0) {
