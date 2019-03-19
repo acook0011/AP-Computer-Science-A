210a9c61d0c22da28b97dcbc73b15113ebc91a3a
Avery Cook <acook0011@myduneland.org>
2019-03-19 12:23:22 -0500

    ArrayLists

---
+++ Chapter14/Labwork/ReadNums.java
@@ -0,0 +1,48 @@
+package Chapter14.Labwork;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * Program reads a file and displays the numbers of that file as a list.
+ *
+ * @aj
+ * @lab2
+ */
+
+public class ReadNums
+{
+    public static void main(String [] args) throws IOException{
+        Scanner reader = new Scanner(new File("readnumb.txt"));
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        
+        // Sets ArrayList
+        while (reader.hasNext()){
+            nums.add(reader.nextInt());
+        }
+        
+        // Finds the average
+        Integer sum = 0;
+        Integer count = 0;
+        for (int i : nums){
+            sum += i;
+            count++;
+        }
+        Integer average = sum/count;
+        System.out.println("Average:   " + average);
+        
+        // Finds the lowest and highest
+        Collections.sort(nums);
+        System.out.println("Highest #: " + nums.get(nums.size()-1));
+        System.out.println("Lowest #:  " + nums.get(0));
+        
+        // Filters out even numbers
+        for (int i = 0; i < nums.size(); i++){
+            if (nums.get(i) % 2 == 0){
+                nums.remove(i);
+                i--;
+            }
+        }
+        System.out.println("Odd numbers: " + nums);
+    }
+}
---
+++ Chapter14/Labwork/ReadWords.java
@@ -0,0 +1,68 @@
+package Chapter14.Labwork;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * Program reads a file and displays the words of that file as a list.
+ *
+ * @aj
+ * @lab1
+ */
+
+public class ReadWords
+{
+    public static void main(String [] args) throws IOException { 
+        Scanner reader = new Scanner(new File("readtext.txt"));
+        ArrayList<String> words = new ArrayList<String>();
+        
+        // Sets ArrayList
+        while (reader.hasNext()){
+            words.add(reader.nextLine());
+        }
+        
+        System.out.println("~Unsorted ArrayList~");
+        for (String w : words){
+            System.out.println(w);
+        }
+        
+        // Sorts in backwards alphabetical order
+        Collections.sort(words, Collections.reverseOrder());
+        System.out.println("~ArrayList sorted in reverse alphabetical order~");
+        // Prints
+        for (String w : words){
+            System.out.println(w);
+        }
+        
+        // Sets first letter in plural words capital
+        for (int i = 0; i < words.size(); i++){
+            String w = words.get(i);
+            String end = w.substring(w.length()-1, w.length());
+            if (end.equals("s") || end.equals("S")){
+                String beg = w.substring(0, 1).toUpperCase();
+                String capital = beg + w.substring(1, w.length());
+                words.set(i, capital);
+            }
+        }
+        // Prints
+        System.out.println("~ArrayList where plural words are capitalized~");
+        for (String w : words){
+            System.out.println(w);
+        }
+        
+        // Removes plural words from the ArrayList
+        for (int i = 0; i < words.size(); i++){
+            String w = words.get(i);
+            String end = w.substring(w.length()-1, w.length());
+            if (end.equals("s") || end.equals("S")){
+                words.remove(i);
+                i--;
+            }
+        }
+        // Prints
+        System.out.println("~ArrayList without plural words~");
+        for (String w: words){
+            System.out.println(w);
+        }
+    }
+}
