52ff8e8b1bd768b709752c9cc0aa9ba8314e1bb6
Avery Cook <acook0011@myduneland.org>
2019-03-20 13:04:48 -0500

    ArrayList

--- Chapter14/Labwork/ShareNums.java
+++ Chapter14/Labwork/ShareNums.java
@@ -12,11 +12,42 @@ import java.util.*;
 public class ShareNums
 {
     public static void main(String [] args){
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        list1.add(1);
+        list1.add(4);
+        list1.add(8);
+        list1.add(9);
+        list1.add(11);
+        list1.add(15);
+        list1.add(17);
+        list1.add(28);
+        list1.add(41);
+        list1.add(59);
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list2.add(4);
+        list2.add(7);
+        list2.add(11);
+        list2.add(17);
+        list2.add(19);
+        list2.add(20);
+        list2.add(23);
+        list2.add(28);
+        list2.add(37);
+        list2.add(59);
+        list2.add(81);
         
+        System.out.println(intersect(list1, list2));
     }
     
     public static ArrayList<Integer> intersect(ArrayList<Integer> a1, ArrayList<Integer> a2){
-        ArrayList<Integer> b1 = new ArrayList<Integer>();
-        return b1;
+        ArrayList<Integer> list3 = new ArrayList<Integer>();
+        for (int i = 0; i < a1.size(); i++){
+            for (int j = 0; j < a2.size(); i++){
+                if (a1.get(i) == a2.get(j)){
+                    list3.add(a1.get(i));
+                }
+            }
+        }
+        return list3;
     }
 }
