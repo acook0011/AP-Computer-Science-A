34f7b95051500c3fcb6d4d268671e69547343acb
Avery Cook <acook0011@myduneland.org>
2019-03-21 11:26:25 -0500

    ArrayList

---
+++ Chapter14/Labwork/FullOfStars.java
@@ -0,0 +1,52 @@
+package Chapter14.Labwork;
+
+import java.util.*;
+
+/**
+ * Add stars to an ArrayList with strings.
+ *
+ * @aj
+ * @lab4 & lab5
+ */
+
+public class FullOfStars
+{
+    public static void main(String [] args){
+        ArrayList<String> swift = new ArrayList<String>();
+        swift.add("the");
+        swift.add("quick");
+        swift.add("brown");
+        swift.add("fox");
+        System.out.println(addStars(swift));
+        System.out.println(removeStars(swift));
+        ArrayList<String> sky = new ArrayList<String>();
+        sky.add("cause");
+        sky.add("you're");
+        sky.add("a");
+        sky.add("sky");
+        sky.add("full");
+        sky.add("of");
+        sky.add("stars");
+        System.out.println(addStars(sky));
+        System.out.println(removeStars(sky));
+    }
+    
+    public static ArrayList<String> addStars(ArrayList<String> a1){
+        // Adds stars
+        for (int i = 0; i < a1.size(); i+=2){
+            a1.add(i+1, "*");
+        }
+        return a1;
+    }
+    
+    public static ArrayList<String> removeStars(ArrayList<String> a1){
+        // Removes stars
+        for (int i = 0; i < a1.size(); i++){
+            if (a1.get(i).equals("*")){
+                a1.remove(i);
+                i--;
+            }
+        }
+        return a1;
+    }
+}
--- Chapter14/Labwork/ShareNums.java
+++ Chapter14/Labwork/ShareNums.java
@@ -14,16 +14,21 @@ public class ShareNums
     public static void main(String [] args){
         ArrayList<Integer> list1 = new ArrayList<Integer>();
         list1.add(1);
+        list1.add(1);
+        list1.add(4);
         list1.add(4);
         list1.add(8);
         list1.add(9);
         list1.add(11);
         list1.add(15);
         list1.add(17);
+        list1.add(17);
+        list1.add(28);
         list1.add(28);
         list1.add(41);
         list1.add(59);
         ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list2.add(4);
         list2.add(4);
         list2.add(7);
         list2.add(11);
@@ -32,8 +37,11 @@ public class ShareNums
         list2.add(20);
         list2.add(23);
         list2.add(28);
+        list2.add(28);
         list2.add(37);
         list2.add(59);
+        list2.add(59);
+        list2.add(81);
         list2.add(81);
         
         System.out.println(intersect(list1, list2));
@@ -41,10 +49,21 @@ public class ShareNums
     
     public static ArrayList<Integer> intersect(ArrayList<Integer> a1, ArrayList<Integer> a2){
         ArrayList<Integer> list3 = new ArrayList<Integer>();
+        // Creates list3 array
         for (int i = 0; i < a1.size(); i++){
-            for (int j = 0; j < a2.size(); i++){
+            for (int j = 0; j < a2.size(); j++){
                 if (a1.get(i) == a2.get(j)){
                     list3.add(a1.get(i));
+                }
+            }
+        }
+        
+        // Removes any duplicates in new ArrayList
+        for (int i = 0; i < list3.size(); i++){
+            for (int j = i+1; j < list3.size(); j++){
+                if (list3.get(i) == list3.get(j)){
+                    list3.remove(j);
+                    j--;
                 }
             }
         }
