b436fd77e6e3a6c2d1f0f92714718ec72d256edf
Avery Cook <acook0011@myduneland.org>
2019-04-02 10:49:29 -0500

    Temperature Sensor

---
+++ Chapter14/TempSense.java
@@ -0,0 +1,21 @@
+package Chapter14;
+
+import java.util.*;
+
+/**
+ * Write a description of class TempSense here.
+ *
+ * @aj
+ * @codewars5
+ */
+public class TempSense
+{
+    public static void main(String [] args){
+        int t;    // Current Sensor's Temperature Value
+        int t0;   // Sensor Calibration that corresponds with C0
+        int t1;   // Sensor Calibration that corresponds with C1
+        int c0;   // Calibration Temperature (degree celsius times 8)
+        int c1;   // Calibration Temperature (degree celsius times 8)
+        
+    }
+}
