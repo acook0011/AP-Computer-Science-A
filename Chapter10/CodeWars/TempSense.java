ae80def0fc4c4897483e9d03b26412daf0588728
Avery Cook <acook0011@myduneland.org>
2019-04-02 11:33:32 -0500

    Temperature Sensor

---
+++ Chapter10/CodeWars/TempSense.java
@@ -0,0 +1,31 @@
+package Chapter10.CodeWars;
+
+/**
+ * Calculates the temperature of a sensor using two calibration values and current sensor value
+ *
+ * @aj
+ * @codewars5
+ */
+
+import java.util.*;
+import java.io.*;
+public class TempSense
+{
+    public static void main(String [] args) throws IOException{
+        Scanner reader = new Scanner(new File("sensors.txt"));
+        int count = reader.nextInt(); // Total calculations to go through
+        
+        while (count != 0){
+            double t  = reader.nextInt();     // Current Sensor's Temperature Value
+            double t0 = reader.nextInt();     // Sensor Calibration that corresponds with C0
+            double t1 = reader.nextInt();     // Sensor Calibration that corresponds with C1
+            double c0 = reader.nextInt()/8.0; // Calibration Temperature
+            double c1 = reader.nextInt()/8.0; // Calibration Temperature
+            double m = (c1-c0)/(t1-t0); // Slope
+            // Equation (point-slope formula): c - c0 = m(t - t0)
+            double c = m*(t-t0) + c0;
+            System.out.println(c); // Answer
+            count--;
+        }
+    }
+}
