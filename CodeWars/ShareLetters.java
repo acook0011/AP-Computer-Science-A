b50878030d733632cfc722f96b9453c5251443af
Avery Cook <acook0011@myduneland.org>
2019-04-22 20:03:02 -0500

    ShareLetters

--- CodeWars/ShareLetters.java
+++ CodeWars/ShareLetters.java
@@ -32,11 +32,63 @@ public class ShareLetters
         if (choice == 1){
             reader = new Scanner(new File("letters1.txt"));
         } else if (choice == 2){
-            reader = new Scanner(new File("letters1.txt"));
+            reader = new Scanner(new File("letters2.txt"));
         } else  {
             System.out.println("! ERROR IN LETTERS CHOICE !");
         }
         
+        int count = reader.nextInt(); reader.nextLine();
         
+        for (int i = count; i > 0; i--){
+            String letters = "";
+            String words = reader.nextLine();
+            // First Word
+            String temp = words.substring(0, words.indexOf(" "));
+            String a = sortString(temp) + " ";
+            // Second Word
+            temp = words.substring(words.indexOf(" ")).trim();
+            String b = sortString(temp.substring(0, temp.indexOf(" "))) + " ";
+            // Third Word
+            temp = temp.substring(temp.indexOf(" ")).trim();
+            String c = sortString(temp) + " ";
+            for (int  x = 0; x < a.length(); x++){ // Picks first letter
+                String compa = a.substring(x, x+1); // Takes letter to compare
+                for (int y = 0; y < b.length(); y++){ // Checks if second word has letter
+                    String compb = b.substring(y, y+1); // Takes letter to compare
+                    if (compb.equals(compa)){
+                        int tempy = y; // To use y later
+                        y = b.length(); // Stop for loop
+                        for (int z = 0; z < c.length(); z++){ // Checks if third word does too
+                            String compc = c.substring(z, z+1); // Takes letter to compare
+                            if (compc.equals(compa)){
+                                int tempz = z; // To use z later
+                                z = c.length(); // Stop for loop
+                                letters += compa; // Adds to result to print
+                                // Cuts out of original b for next check
+                                if (b.length() == tempy){
+                                    b = b.substring(0, tempy);
+                                } else {
+                                    b = b.substring(0, tempy) + b.substring(tempy+1);
+                                }
+                                // Cuts out of original c for next check
+                                if (c.length() == tempz){
+                                    c = c.substring(0, tempz);
+                                } else {
+                                    c = c.substring(0, tempz) + c.substring(tempz+1);
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            System.out.println(letters);
+        }
     }
+    
+    public static String sortString(String inputString) { 
+        char tempArray[] = inputString.toCharArray(); 
+        Arrays.sort(tempArray); 
+        return new String(tempArray); 
+    } 
 }
